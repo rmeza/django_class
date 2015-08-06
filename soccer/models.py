cdedd638dc0028b2e882b3b58b412203f220144f
invitado <invitado@debian>
2015-08-06 13:53:06 -0500

    code review

--- soccer/models.py
+++ soccer/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Team(models.Model):
 	"""
 	my doc string
@@ -9,10 +8,12 @@ class Team(models.Model):
 
 	def __unicode__(self):
 		return self.name
-	
 
 
 class Player(models.Model):
+	"""
+	my doc string
+	"""
 	name=models.CharField(max_length=200)
 	team=models.ForeignKey(Team)
 
